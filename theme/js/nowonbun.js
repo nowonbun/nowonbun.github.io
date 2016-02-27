c3ed72f8852c1de633c6eed321a8ca4810c75928
nowonbun <nowonbun@naver.com>
2016-02-27 23:55:46 +0900

    자바 스크립트 정리

---
+++ theme/js/nowonbun.js
@@ -0,0 +1,38 @@
+<!--분류 전체보기 삭제-->
+$("ul.tt_category > li > a").hide();
+<!--메뉴 바로가기 설정-->
+$("ul.category_list > li > a > img").hide();
+$("ul.category_list > li").prop("class","dropdown");
+$("ul.category_list > li > a").prop("class","dropdown-toggle");
+$("ul.sub_category_list").prop("class","dropdown-menu");
+console.log($("ul.tt_category").html());
+$("ul.navbar-nav").html($("ul.tt_category > li  > ul.category_list").html());
+$("#webType").val("0");
+if($(".navbar-toggle").css("display") === "block"){
+	$("li.dropdown > a").prop("href","#");
+	$("li.dropdown > a").attr("data-toggle","dropdown");
+	$("#webType").val("1");
+}
+$(window).resize(function() {
+	$("li.open").prop("class","dropdown");
+	if($(".navbar-toggle").css("display") === "block"){
+		if($("#webType").val()  !== "1" ){
+			$("ul.navbar").html($("ul.tt_category").html());
+			
+			$("li.dropdown > a").prop("href","#");
+			$("li.dropdown > a").attr("data-toggle","dropdown");
+			$("#webType").val("1");
+		}
+	}else{
+		if($("#webType").val()  !== "0" ){
+			$("ul.navbar").html($("ul.tt_category").html());
+			$("#webType").val("0");
+		}
+	}
+});
+$(".dropdown-menu").hover(function(){
+	$(this).parent().css("color","#fff");
+	$(this).parent().css("background-color","#f1c40f");
+},function(){
+	$(this).parent().css("background-color","#29343a");
+});
