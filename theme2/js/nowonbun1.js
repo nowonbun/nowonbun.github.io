dd9373191106374288c37a3bb22bcd68e50752ef
nowonbun <nowonbun@naver.com>
2016-03-09 15:59:58 +0900

    재 개발중
    재 개발중

--- theme2/js/nowonbun1.js
+++ theme2/js/nowonbun1.js
@@ -1,19 +1,16 @@
 $(function(){
-		temp = $("ul.category_list").html();
-		$("ul.top-nav").html(temp);
-		$("ul.side-nav").html(temp);
-		$("ul.navbar-nav ul").addClass("none");
-		$("ul.navbar-nav > li > a").prop("href","#");
-		$("ul.navbar-nav > li > a").addClass("dropdown");
-		$("a.dropdown").bind("click",function(){
-			dom = $(this).parent().children("ul");
-			if(dom.hasClass("none")){
-				dom.removeClass("none");
-			}else{
-				dom.addClass("none");
-			}
-		});
-		$(window).on('scroll', function() {
-			fixScrollMenu();
-		});
+	temp = $("ul.category_list").html();
+	$("ul.top-nav").html(temp);
+	$("ul.side-nav").html(temp);
+	$("ul.navbar-nav ul").addClass("none");
+	$("ul.navbar-nav > li > a").prop("href","#");
+	$("ul.navbar-nav > li > a").addClass("dropdown");
+	$("a.dropdown").bind("click",function(){
+		dom = $(this).parent().children("ul");
+		if(dom.hasClass("none")){
+			dom.removeClass("none");
+		}else{
+			dom.addClass("none");
+		}
 	});
+});
