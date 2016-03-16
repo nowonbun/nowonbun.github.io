a7d924d239d4352570e5934b76e01accb9d84fb0
nowonbun <nowonbun@naver.com>
2016-03-16 16:44:03 +0900

    티스토리 소스 보기 버그?
    티스토리 소스 보기 버그?

--- post/318/Calculator.java
+++ post/318/Calculator.java
@@ -4,25 +4,24 @@ import java.util.List;
 import java.util.Stack;
 
 public class Calculator {
-	/*연산자가 아닌 기호*/
+	//연산자가 아닌 기호
 	private final String[] OPERATION0 = { "(", ")", "," };
-	/*수 한 개가 필요한 연산기호(수는 왼쪽에 배치)*/
+	//수 한 개가 필요한 연산기호(수는 왼쪽에 배치)
 	private final String[] OPERATION1 = { "!" };
-	/*수 두 개가 필요한 연산기호(수는 양옆에 배치) - 왼쪽에서 오른쪽으로 계산한다.
-	 * 예) 1 + 2 = 3, 6 / 3 = 2, 2 ^ 3 = 8..
-	 * */
+	//수 두 개가 필요한 연산기호(수는 양옆에 배치) - 왼쪽에서 오른쪽으로 계산한다.
+	//예) 1 + 2 = 3, 6 / 3 = 2, 2 ^ 3 = 8..
 	private final String[] OPERATION2 = { "+", "-", "*", "/", "^", "%" };
-	/*수가 필요없는 문자 연산기호*/
+	//수가 필요없는 문자 연산기호
 	private final String[] WORD_OPERATION1 = { "pi", "e" };
-	/*수 한 개가 필요한 문자 연산기호(괄호로 구분한다.)*/
+	//수 한 개가 필요한 문자 연산기호(괄호로 구분한다.)
 	private final String[] WORD_OPERATION2 = { "sin", "sinh", "asin", "cos", "cosh", "acos", "tan", "tanh", "atan",
 			"sqrt", "exp", "abs", "log", "ceil", "floor", "round" };
-	/*수 두 개가 필요한 문자 연산기호(괄호, 콤마로 구분한다.)*/
+	//수 두 개가 필요한 문자 연산기호(괄호, 콤마로 구분한다.)
 	private final String[] WORD_OPERATION3 = { "pow" };
-	/*나누기할 때 반올림 자릿수*/
+	//나누기할 때 반올림 자릿수
 	private int HARF_ROUND_UP = 6;
 
-	/*싱글톤*/
+	//싱글톤
 	private static Calculator instance = null;
 	/**
 	 * 생성자
@@ -51,28 +50,26 @@ public class Calculator {
 	 * @return 결과 값
 	 */
 	private BigDecimal calc(String data) {
-		/*계산 식 안의 빈칸을 없앤다.*/
+		//계산 식 안의 빈칸을 없앤다.
 		data = data.replace(" ", "");
-		/*토큰으로 구분,즉 구분되는 수, 구분을 모두 분할
-		 * 예) (10+2)*(3+4)의 경우는 (, 10, +, 2, ), *, (, 3, +, 4, )로 분할 된다.
-		 * */
-		List<Object> tokenStack = makeTokens(data);
-		/* 후위 표기식으로 변환한다.
-		 * 예) (, 10, +, 2, ), *, (, 3, +, 4, )의 경우는 10 2 + 3 4 + * 로 변경
-		 */
+		//토큰으로 구분,즉 구분되는 수, 구분을 모두 분할
+		// 예) (10+2)*(3+4)의 경우는 (, 10, +, 2, ), *, (, 3, +, 4, )로 분할 된다.
+		List< Object > tokenStack = makeTokens(data);
+		// 후위 표기식으로 변환한다.
+		// 예) (, 10, +, 2, ), *, (, 3, +, 4, )의 경우는 10 2 + 3 4 + * 로 변경
 		tokenStack = convertPostOrder(tokenStack);
-		Stack<Object> calcStack = new Stack<Object>();
-		/* 후위 표기식 계산
-		 * List형식으로 tocken이 수가 나오면 스택, 연산자가 나오면 계산을 합니다.
-		 * 10 2 + 계산 12
-		 * 12 3 4 + 계산 12 7
-		 * 12 * 7 계산 84
-		 * */
+		Stack< Object > calcStack = new Stack< Object >();
+		// 후위 표기식 계산
+		// List형식으로 tocken이 수가 나오면 스택, 연산자가 나오면 계산을 합니다.
+		// 10 2 + 계산 12
+		// 12 3 4 + 계산 12 7
+		// 12 * 7 계산 84
+		//
 		for (Object tocken : tokenStack) {
 			calcStack.push(tocken);
 			calcStack = calcPostOrder(calcStack);
 		}
-		/*스택에 값이 없으면 에러..*/
+		//스택에 값이 없으면 에러
 		if (calcStack.size() != 1) {
 			throw new RuntimeException("Calulator Error");
 		}
@@ -83,25 +80,25 @@ public class Calculator {
 	 * @param calcStack 스택에 담겨져 있는 값
 	 * @return
 	 */
-	private Stack<Object> calcPostOrder(Stack<Object> calcStack) {
-		/*스택의 가장 위의 값이 수면 계산 안함*/
+	private Stack< Object > calcPostOrder(Stack< Object > calcStack) {
+		//스택의 가장 위의 값이 수면 계산 안함
 		if (calcStack.lastElement().getClass().equals(BigDecimal.class)) {
 			return calcStack;
 		}
 		BigDecimal op1 = null;
 		BigDecimal op2 = null;
 		String opcode = null;
-		/*연산자 포함 스택에 최소 2개 이상*/
+		//연산자 포함 스택에 최소 2개 이상
 		if (calcStack.size() >= 2) {
-			/*스택의 가장 위는 연산자*/
+			//스택의 가장 위는 연산자
 			opcode = (String) calcStack.pop();
-			/*다음 밑은 수*/
+			//다음 밑은 수
 			op1 = (BigDecimal) calcStack.pop();
-			/*연산자가 수를 1개 필요한지 2개 필요한지 체크*/
+			//연산자가 수를 1개 필요한지 2개 필요한지 체크
 			if (!opCodeCheck(opcode)) {
 				op2 = (BigDecimal) calcStack.pop();
 			}
-			/*계산*/
+			//계산
 			BigDecimal result = calculateByOpCode(op1, op2, opcode);
 			calcStack.push(result);
 		}
@@ -124,25 +121,25 @@ public class Calculator {
 	 */
 	private BigDecimal calculateByOpCode(BigDecimal op1, BigDecimal op2, String opcode) {
 		if (OPERATION2[0].equals(opcode)) {
-			/*더하기*/
+			//더하기
 			return op1.add(op2);
 		} else if (OPERATION2[1].equals(opcode)) {
-			/*빼기*/
+			//빼기
 			return op1.subtract(op2);
 		} else if (OPERATION2[2].equals(opcode)) {
-			/*곱하기*/
+			//곱하기
 			return op1.multiply(op2);
 		} else if (OPERATION2[3].equals(opcode)) {
-			/*나누기, 반올림은 지정된 수*/
+			//나누기, 반올림은 지정된 수
 			return op2.divide(op1, HARF_ROUND_UP, BigDecimal.ROUND_HALF_UP);
 		} else if (OPERATION2[4].equals(opcode)) {
-			/*제곱*/
+			//제곱
 			return op2.pow(op1.intValue());
 		} else if (OPERATION2[5].equals(opcode)) {
-			/*나머지*/
+			//나머지
 			return op2.remainder(op1);
 		} else if (OPERATION1[0].equals(opcode)) {
-			/*팩토리얼*/
+			//팩토리얼
 			return Factorial(op1);
 		} else if (WORD_OPERATION2[0].equals(opcode)) {
 			return BigDecimal.valueOf(Math.sin(op1.doubleValue()));
@@ -197,21 +194,21 @@ public class Calculator {
 	 * @param tokenList 토큰 리스트
 	 * @return
 	 */
-	private List<Object> convertPostOrder(List<Object> tokenList) {
-		List<Object> postOrderList = new ArrayList<>();
+	private List< Object > convertPostOrder(List< Object > tokenList) {
+		List< Object > postOrderList = new ArrayList<>();
 		Stack<String> exprStack = new Stack<>();
 		Stack<String> wordStack = new Stack<>();
 		for (Object token : tokenList) {
 			if (BigDecimal.class.equals(token.getClass())) {
-				/*수면 그대로 입력*/
+				//수면 그대로 입력
 				postOrderList.add(token);
 			} else {
-				/*연산자 처리*/
+				//연산자 처리
 				exprAppend((String) token, exprStack, wordStack, postOrderList);
 			}
 		}
 		String item = null;
-		/*남은 연산자 넣기*/
+		//남은 연산자 넣기
 		while (!exprStack.isEmpty()) {
 			item = exprStack.pop();
 			postOrderList.add(item);
@@ -227,10 +224,10 @@ public class Calculator {
 	 * @return
 	 */
 	private void exprAppend(String token, Stack<String> exprStack, Stack<String> wordStack,
-			List<Object> postOrderList/*REFERENCE*/) {
-		/*토큰이 문자일 경우처리*/
+			List< Object > postOrderList) {
+		//토큰이 문자일 경우처리
 		if (isWordOperation(token)) {
-			/*PI, E의 값*/
+			//PI, E의 값
 			BigDecimal wordValue = ConverterWordResult(token);
 			if (wordValue != null) {
 				postOrderList.add(wordValue);
@@ -238,31 +235,31 @@ public class Calculator {
 				wordStack.push(token);
 			}
 		} else if (OPERATION0[0].equals(token)) {
-			/*왼쪽 괄호( */
+			//왼쪽 괄호( 
 			exprStack.push(token);
 		} else if (OPERATION0[1].equals(token)) {
-			/*오른쪽 괄호) */
+			//오른쪽 괄호) 
 			String opcode = null;
 			while (true) {
-				/*문자 스택이 없을 때 까지*/
+				//문자 스택이 없을 때 까지
 				if (wordStack.size() > 0) {
-					/*기호를 스택에서 가져온다.*/
+					//기호를 스택에서 가져온다.
 					opcode = exprStack.pop();
-					/*왼쪽 괄호(를 만나면 작성 끝 */
+					//왼쪽 괄호(를 만나면 작성 끝 
 					if (OPERATION0[0].equals(opcode)) {
 						opcode = wordStack.pop();
 						postOrderList.add(opcode);
 						break;
 					}
-					/*스택 순서로 후위 계산 리스트에 값을 넣는다.*/
+					//스택 순서로 후위 계산 리스트에 값을 넣는다.
 					postOrderList.add(opcode);
 				} else {
-					/*연산 스택이 없으면 종료*/
+					//연산 스택이 없으면 종료
 					if (exprStack.size() < 1) {
 						break;
 					}
 					opcode = exprStack.pop();
-					/*왼쪽 괄호(를 만나면 작성 끝 */
+					//왼쪽 괄호(를 만나면 작성 끝 
 					if (OPERATION0[0].equals(opcode)) {
 						break;
 					}
@@ -270,18 +267,18 @@ public class Calculator {
 				}
 			}
 		} else if (OPERATION0[2].equals(token)) {
-			/*콤마 처리*/
-			/*콤마는 문자 연산자와 같이 사용하므로 콤마 연산자가 나왔는데 문자 연산자가 없으면 에러*/
+			//콤마 처리
+			//콤마는 문자 연산자와 같이 사용하므로 콤마 연산자가 나왔는데 문자 연산자가 없으면 에러
 			if (wordStack.size() < 1) {
 				throw new RuntimeException("data error");
 			}
 			String opcode = null;
 			while (true) {
-				/*연산 스택이 없으면 종료*/
+				//연산 스택이 없으면 종료
 				if (exprStack.size() < 1) {
 					break;
 				}
-				/*왼쪽 괄호면 종료*/
+				//왼쪽 괄호면 종료
 				if (OPERATION0[0].equals(exprStack.lastElement())) {
 					break;
 				}
@@ -289,17 +286,17 @@ public class Calculator {
 				postOrderList.add(opcode);
 			}
 		} else if (isOperation(token)) {
-			/*연산자 처리*/
+			//연산자 처리
 			String opcode = null;
 			while (true) {
-				/*연산자가 없으면 입력*/
+				//연산자가 없으면 입력
 				if (exprStack.isEmpty()) {
 					exprStack.push(token);
 					break;
 				}
-				/*연산자가 있으면*/
+				//연산자가 있으면
 				opcode = exprStack.pop();
-				/*연산자 우선순위 체크 + * 가 만나면 *계산 먼저(스택에 늦게 들어가는 게 FIFO법칙으로 먼저 계산됨)*/
+				//연산자 우선순위 체크 + * 가 만나면 *계산 먼저(스택에 늦게 들어가는 게 FIFO법칙으로 먼저 계산됨)
 				if (exprOrder(opcode) <= exprOrder(token)) {
 					exprStack.push(opcode);
 					exprStack.push(token);
@@ -314,30 +311,30 @@ public class Calculator {
 	 * @param inputData
 	 * @return
 	 */
-	private List<Object> makeTokens(String inputData) {
-		List<Object> tokenStack = new ArrayList<>();
+	private List< Object > makeTokens(String inputData) {
+		List< Object > tokenStack = new ArrayList<>();
 		StringBuffer numberTokenBuffer = new StringBuffer();
 		StringBuffer wordTokenBuffer = new StringBuffer();
 		int argSize = inputData.length();
 		char token;
 		for (int i = 0; i < argSize; i++) {
-			/*char형식으로 분할*/
+			//char형식으로 분할
 			token = inputData.charAt(i);
-			/*수 토큰*/
+			//수 토큰
 			if (!isOperation(token)) {
-				/*문자열이 있으면 넣는다.*/
+				//문자열이 있으면 넣는다.
 				setWordOperation(tokenStack, wordTokenBuffer);
 				numberTokenBuffer.append(token);
 				if (i == argSize - 1) {
 					setNumber(tokenStack, numberTokenBuffer);
 				}
 			} else {
-				/*연산자면 기존의 수를 입력*/
+				//연산자면 기존의 수를 입력
 				setNumber(tokenStack, numberTokenBuffer);
 				if (setOperation(tokenStack, token)) {
 					continue;
 				}
-				/*기호 연산자가 아니면 문자 연산자*/
+				//기호 연산자가 아니면 문자 연산자
 				wordTokenBuffer.append(token);
 				setWordOperation(tokenStack, wordTokenBuffer);
 			}
@@ -350,7 +347,7 @@ public class Calculator {
 	 * @param token
 	 * @return
 	 */
-	private boolean setOperation(List<Object> tokenStack, char token) {
+	private boolean setOperation(List< Object > tokenStack, char token) {
 		String tokenBuffer = Character.toString(token);
 		if (containWord(tokenBuffer, OPERATION2) || containWord(tokenBuffer, OPERATION1)
 				|| containWord(tokenBuffer, OPERATION0)) {
@@ -364,7 +361,7 @@ public class Calculator {
 	 * @param tokenStack
 	 * @param tokenBuffer
 	 */
-	private void setWordOperation(List<Object> tokenStack, StringBuffer tokenBuffer) {
+	private void setWordOperation(List< Object > tokenStack, StringBuffer tokenBuffer) {
 		if (isWordOperation(tokenBuffer)) {
 			tokenStack.add(tokenBuffer.toString());
 			tokenBuffer.setLength(0);
@@ -375,7 +372,7 @@ public class Calculator {
 	 * @param tokenStack
 	 * @param tokenBuffer
 	 */
-	private void setNumber(List<Object> tokenStack, StringBuffer tokenBuffer) {
+	private void setNumber(List< Object > tokenStack, StringBuffer tokenBuffer) {
 		if (tokenBuffer.length() > 0) {
 			BigDecimal number = new BigDecimal(tokenBuffer.toString());
 			tokenStack.add(number);
