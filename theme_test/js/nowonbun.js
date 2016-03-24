86c1481041cc04cb19d97837fba340e895006606
nowonbun <nowonbun@naver.com>
2016-03-25 02:03:06 +0900

    왼쪽 메뉴 조정

--- theme_test/js/nowonbun.js
+++ theme_test/js/nowonbun.js
@@ -218,7 +218,7 @@ function sideLeftOn(){
 	//$("aside#leftside").css("width",$(window).width());
 	$("aside#leftside").css("left",0);
 	/*하단 아이콘처리*/
-	$("aside#leftside > div.side-list").css("min-height",$("aside#leftside").height()-435);
+	$("aside#leftside > div.side-list").css("min-height",$("aside#leftside").height()-210);
 	changeClassState($("main"),true);
 	changeClassState($("aside#leftside"),true);
 }
