ffd440b87d020c936b0b031607243d18548bcf4b
nowonbun <nowonbun@naver.com>
2016-03-26 19:39:17 +0900

    메인 화면 리다이렉트 분리

--- japanbunTheme/js/nowonbun.js
+++ japanbunTheme/js/nowonbun.js
@@ -4,16 +4,6 @@ var preWidth = 0;
 var rightsideListCount = 9;
 var myUrl = "http://japanbun.tistory.com";
 
-/*메인 화면 리다이렉트*/
-var url = decodeURIComponent(location.href);
-url = url.replace('http://','').replace('https://','');
-url = url.split('#')[0];
-url = url.split('?')[0];
-url = url.split('/');
-if(url[1] == '') { 
-	location.href = '/notice/2'; 
-}
-
 /*OnLoad*/
 initLogo();
 initMain();
