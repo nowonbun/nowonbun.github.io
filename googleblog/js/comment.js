231dca7b887b1dc21cd5024073293860bc1e53d3
nowonbun <nowonbun@naver.com>
2016-04-18 23:46:37 +0900

    스크립트 수정

--- googleblog/js/comment.js
+++ googleblog/js/comment.js
@@ -1,7 +1,7 @@
 (function() {
-	var items = <data:post.commentJso/>;
-	var msgs = <data:post.commentMsgs/>;
-	var config = <data:post.commentConfig/>;
+	var items = $('#commentJao').val();
+	var msgs = $('#commentMsgs').val();
+	var config = $('#commentConfig').val();
 	var cursor = null;
 	if (items && items.length> 0) {
 		cursor = parseInt(items[items.length - 1].timestamp) + 1;
