fe57181776cd2296e21ec9244c7422ea6e057370
nowonbun@naver.com <ghkdtnsduq1>
2017-02-20 21:52:10 +0900

    source clean up

--- japanbunTheme/js/redirect.js
+++
@@ -1,9 +0,0 @@
-/*메인 화면 리다이렉트*/
-var url = decodeURIComponent(location.href);
-url = url.replace('http://','').replace('https://','');
-url = url.split('#')[0];
-url = url.split('?')[0];
-url = url.split('/');
-if(url[1] == '') { 
-	location.href = '/notice/2'; 
-}
